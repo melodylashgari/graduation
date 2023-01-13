055b9f928a5077f4490788ae216a048eae1c634e
Melody Lashgari <92079697+melodylashgari@users.noreply.github.com>
2023-01-13 12:54:02 +0100

    feat. backend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
+import Navbar from './components/Navbar';
+import Cart from './components/Cart';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
+
 
 function App() {
   return (
     <div className="App">
-    
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='*' element={<NotFound />}></Route>
+          <Route path="/cart" exact element={<Cart />}></Route>
+          <Route path="/" exact element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
