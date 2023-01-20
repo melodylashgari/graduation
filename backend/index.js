a8529f231ede428878e3737e4569237d5cef884a
Melody Lashgari <92079697+melodylashgari@users.noreply.github.com>
2023-01-20 17:25:44 +0100

    feat register user

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const register = require("./routes/register")
 
 const products = require("./products");
 
@@ -10,6 +11,8 @@ require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/register", register)
 
 app.get("/", (req, res) => {
   res.send("Welcome to our online shop API");
