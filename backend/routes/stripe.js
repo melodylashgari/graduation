4d44a8ad6e6b23fe13fb13aa5c170debfbf270d9
Melody Lashgari <92079697+melodylashgari@users.noreply.github.com>
2023-01-23 19:16:26 +0100

    feat creating order in db using stripe webhooks

--- backend/routes/stripe.js
+++ backend/routes/stripe.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Stripe = require("stripe");
+const { Order } = require("../models/order");
 
 require("dotenv").config();
 
@@ -8,7 +9,14 @@ const stripe = Stripe(process.env.STRIPE_KEY);
 const router = express.Router();
 
 router.post("/create-checkout-session", async (req, res) => {
-  const line_items = req.body.cartItems.map(item => {
+  const customer = await stripe.customers.create({
+    metadata: {
+      userId: req.body.userId,
+      cart: JSON.stringify(req.body.cartItems),
+    },
+  });
+
+  const line_items = req.body.cartItems.map((item) => {
     return {
       price_data: {
         currency: "SEK",
@@ -26,57 +34,58 @@ router.post("/create-checkout-session", async (req, res) => {
     };
   });
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
+    payment_method_types: ["card"],
     shipping_address_collection: {
-        allowed_countries: ['US', 'SE'],
+      allowed_countries: ["US", "SE"],
     },
     shipping_options: [
-        {
-            shipping_rate_data: {
-                type: 'fixed_amount',
-                fixed_amount: {
-                    amount: 0,
-                    currency: 'SEK',
-                },
-                display_name: 'Free shipping',
-                // Delivers between 5-7 business days
-                delivery_estimate: {
-                    minimum: {
-                        unit: 'business_day',
-                        value: 5,
-                    },
-                    maximum: {
-                        unit: 'business_day',
-                        value: 7,
-                    }
-                } 
-            }
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: 0,
+            currency: "SEK",
+          },
+          display_name: "Free shipping",
+          // Delivers between 5-7 business days
+          delivery_estimate: {
+            minimum: {
+              unit: "business_day",
+              value: 5,
+            },
+            maximum: {
+              unit: "business_day",
+              value: 7,
+            },
+          },
         },
-        {
-            shipping_rate_data: {
-                type: 'fixed_amount',
-                fixed_amount: {
-                    amount: 7500,
-                    currency: 'SEK'
-                },
-                display_name: 'Next day air',
-                // Delivers in exactly 1 business day
-                delivery_estimate: {
-                    minimum: {
-                        unit: 'business_day',
-                        value: 1,
-                    },
-                    maximum: {
-                        unit: 'business_day',
-                        value: 1,
-                    }
-                }
-            }
-        }
+      },
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: 7500,
+            currency: "SEK",
+          },
+          display_name: "Next day air",
+          // Delivers in exactly 1 business day
+          delivery_estimate: {
+            minimum: {
+              unit: "business_day",
+              value: 1,
+            },
+            maximum: {
+              unit: "business_day",
+              value: 1,
+            },
+          },
+        },
+      },
     ],
     phone_number_collection: {
-        enabled: true,
+      enabled: true,
     },
+    customer: customer.id,
     line_items,
     mode: "payment",
     success_url: `${process.env.CLIENT_URL}/checkout-success`,
@@ -85,5 +94,73 @@ router.post("/create-checkout-session", async (req, res) => {
 
   res.send({ url: session.url });
 });
+
+// Create Order
+const createOrder = async (customer, data) => {
+  const Items = JSON.parse(customer.metadata.cart);
+  const newOrder = new Order({
+    userId: customer.metadata.userId,
+    customerId: data.customer,
+    paymentIntentId: data.payment_intent,
+    products: Items,
+    subtotal: data.amount_subtotal,
+    total: data.amount_total,
+    shipping: data.customer_details,
+    payment_status: data.payment_status,
+  });
+  try{
+    const savedOrder = await newOrder.save()
+    console.log("Processed Order:", savedOrder);
+    //email
+  }catch(err){
+    console.log(err)
+  }
+};
+
+let endpointSecret;
+
+// endpointSecret = "whsec_f4c55332bc1216488a5c67f15f4bbd2bcd1b132f8b64747461ded2340bb7ed10";
+
+router.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (req, res) => {
+    const sig = req.headers["stripe-signature"];
+
+    let data;
+    let eventType;
+
+    if (endpointSecret) {
+      let event;
+
+      try {
+        event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
+        console.log("Webhook verified.");
+      } catch (err) {
+        console.log(`Webhook Error: ${err.message}`);
+        res.status(400).send(`Webhook Error: ${err.message}`);
+        return;
+      }
+      data = event.data.object;
+      eventType = event.type;
+    } else {
+      data = req.body.data.object;
+      eventType = req.body.type;
+    }
+
+    // Handle the event
+    if (eventType === "checkout.session.completed") {
+      stripe.customers
+        .retrieve(data.customer)
+        .then((customer) => {
+          createOrder(customer, data);
+        })
+        .catch((err) => console.log(err.message));
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    res.send().end();
+  }
+);
 
 module.exports = router;
